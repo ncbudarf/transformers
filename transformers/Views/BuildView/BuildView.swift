4d51c07624ecc0c592461feae71e862db5cc8cfb
Noah Budarf <ncbudarf@gmail.com>
2020-06-24 16:41:06 -0700

    Basics of the Build tab are done

--- transformers/Views/BuildView/BuildView.swift
+++ transformers/Views/BuildView/BuildView.swift
@@ -11,7 +11,98 @@ import SwiftUI
 struct BuildView: View {
     @ObservedObject var viewModel: BuildViewModel = BuildViewModel()
     
+    private func factionSelection() {
+        
+    }
+    
+    private func rerollRobot() {
+        
+    }
+    
+    private func submitRobot() {
+        
+    }
+    
     var body: some View {
-        PieChartView(pieChart: viewModel.pieChart)
+        GeometryReader { geometry in
+            ScrollView( .vertical) {
+                VStack() {
+                    HStack {
+                        Button(action: self.factionSelection) {
+                            Image("autobotButtonIcon")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundColor(Color.red)
+                                .padding()
+                        }.padding()
+                        Button(action: self.factionSelection) {
+                            Image("decepticonButtonIcon")
+                                .resizable()
+                                .foregroundColor(Color.purple)
+                                .scaledToFit().padding()
+                        }.padding()
+                    }.frame(width: geometry.size.width, height: 150)
+                    
+                    ZStack {
+                        PieChartView(pieChart: self.viewModel.pieChart)
+                            .frame(width: geometry.size.width/1.5, height: geometry.size.width/1.5)
+                        
+                        Text(self.viewModel.totalPowerText())
+                            .font(Font.system(size:30))
+                            .frame(width: geometry.size.width, height: geometry.size.width/1.5, alignment: .bottom)
+                            .foregroundColor(.purple)//TODO: color should change on faction selection
+                    }
+            
+                    HStack {
+                        Button(action: self.rerollRobot) {
+                            Text(self.viewModel.reRollButtonText())
+                                .font(Font.system(size:25))
+                                .fontWeight(.bold)
+                                .padding()
+                                .font(.title)
+                                .foregroundColor(.purple)//TODO: color should change on faction selection
+                        }.padding()
+                        
+                        Button(action: self.submitRobot) {
+                            Text(self.viewModel.submitButtonText())
+                                .font(Font.system(size:25))
+                                .fontWeight(.bold)
+                                .padding()
+                                .font(.title)
+                                .foregroundColor(.purple)//TODO: color should change on faction selection
+                        }.padding()
+                        
+                    }.frame(width: geometry.size.width, height: 50)
+                }
+            }
+        }
+    }
+}
+
+struct StatView: View {
+    var statViewModel: StatViewModel
+    
+    var body: some View {
+        HStack {
+            Text("\(statViewModel.statName):").padding()
+            ZStack {
+                Rectangle()
+                    .fill(statViewModel.statColor)
+                    .frame(width: 25, height: 25)
+                Text(String(statViewModel.statValue)).padding()
+            }
+        }
+    }
+}
+
+struct StatViewModel {
+    let statName: String
+    var statValue: Int
+    var statColor: Color
+    
+    init(statName: String, statValue: Int, statColor: Color) {
+        self.statName = statName
+        self.statValue = statValue
+        self.statColor = statColor
     }
 }
--- transformers/Views/BuildView/BuildViewModel.swift
+++ transformers/Views/BuildView/BuildViewModel.swift
@@ -11,8 +11,31 @@ import Foundation
 class BuildViewModel: ObservableObject {
     var newTransformer: TransformerToCreate = TransformerToCreate()
     var pieChart: PieChart
+
     
     init() {
         self.pieChart = PieChart(newTransformer: newTransformer)
+    }
+}
+
+extension BuildViewModel {
+    func currentTransformerPower() -> Int {
+        return newTransformer.strength + newTransformer.intelligence + newTransformer.speed + newTransformer.endurance + newTransformer.firepower
+    }
+    
+    func totalPowerText() -> String {
+        return "Total Power: \(currentTransformerPower())"
+    }
+}
+
+extension BuildViewModel {
+    func reRollButtonText() -> String {
+        return "Reroll"
+    }
+}
+
+extension BuildViewModel {
+    func submitButtonText() -> String {
+        return "Submit"
     }
 }
